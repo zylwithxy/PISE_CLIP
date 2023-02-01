bfae4352aa5eb28f25fbda8f341b1578195819d1
XUE Yu <2749964428@qq.com>
2023-02-01 21:52:39 +0800

    print the total epoch numbers

--- util/visualizer.py
+++ util/visualizer.py
@@ -136,8 +136,8 @@ class Visualizer():
         )
 
     # errors: same format as |errors| of plotCurrentErrors
-    def print_current_errors(self, epoch, i, errors, t):
-        message = '(epoch: %d, iters: %d, time: %.3f) ' % (epoch, i, t)
+    def print_current_errors(self, epoch, total_epoch, i, errors, t):
+        message = '(epoch: %d/%d, iters: %d, time: %.3f) ' % (epoch, total_epoch, i, t)
         for k, v in errors.items():
             message += '%s: %.3f ' % (k, v)
 
